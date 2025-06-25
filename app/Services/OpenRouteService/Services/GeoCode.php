4293d67fc6c4061c789ea1ffcde0c11650de0c8b
AxelReviron <axel-reviron@proton.me>
2025-06-25 17:29:56 +0200

    Add reverse search

--- app/Services/OpenRouteService/Services/GeoCode.php
+++ app/Services/OpenRouteService/Services/GeoCode.php
@@ -5,6 +5,7 @@ namespace App\Services\OpenRouteService\Services;
 use App\Services\OpenRouteService\Client;
 use App\Services\OpenRouteService\Dto\LocationFeatureDTO;
 use Illuminate\Http\Client\ConnectionException;
+use Illuminate\Http\Client\RequestException;
 use Illuminate\Support\Collection;
 
 class GeoCode
@@ -20,13 +21,33 @@ class GeoCode
      *
      * @param string $place
      * @return Collection<LocationFeatureDTO>
-     * @throws ConnectionException
+     * @throws ConnectionException|RequestException
      */
     public function search(string $place): Collection
     {
         $response = $this->client->get(
             endpoint: '/geocode/search',
             params: ['text' => $place]
+        );
+
+        return collect($response['features'])->map(fn ($feature) => LocationFeatureDTO::fromArray($feature));
+    }
+
+    /**
+     * Resolve input coordinates into address.
+     *
+     * @param array<float> $coordinates
+     * @return Collection<LocationFeatureDTO>
+     * @throws ConnectionException|RequestException
+     */
+    public function reverseSearch(array $coordinates): Collection
+    {
+        $response = $this->client->get(
+            endpoint: '/geocode/reverse',
+            params: [
+                'point.lon' => $coordinates['lon'],
+                'point.lat' => $coordinates['lat'],
+            ]
         );
 
         return collect($response['features'])->map(fn ($feature) => LocationFeatureDTO::fromArray($feature));
