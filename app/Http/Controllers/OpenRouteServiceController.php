c507482660dcef87d6306c95f9bd69ab7d6fd61c
AxelReviron <axel-reviron@proton.me>
2025-06-26 09:53:34 +0200

    use form requests

--- app/Http/Controllers/OpenRouteServiceController.php
+++ app/Http/Controllers/OpenRouteServiceController.php
@@ -2,27 +2,68 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\OpenRouteService\GetRouteRequest;
+use App\Http\Requests\OpenRouteService\ReverseSearchRequest;
+use App\Http\Requests\OpenRouteService\SearchPlaceRequest;
+use App\Services\OpenRouteService\Services\Directions;
 use App\Services\OpenRouteService\Services\GeoCode;
+use Illuminate\Http\Client\ConnectionException;
+use Illuminate\Http\Client\RequestException;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 
 class OpenRouteServiceController extends Controller
 {
     private GeoCode $geoCodeService;
+    private Directions $directionsService;
 
-    public function __construct(GeoCode $geoCodeService)
+    public function __construct(GeoCode $geoCodeService, Directions $directionsService)
     {
         $this->geoCodeService = $geoCodeService;
+        $this->directionsService = $directionsService;
     }
 
-    public function searchPlace(string $place): JsonResponse
+    /**
+     * Search place by name.
+     *
+     * @param SearchPlaceRequest $request
+     * @return JsonResponse
+     * @throws ConnectionException|RequestException
+     */
+    public function searchPlace(SearchPlaceRequest $request): JsonResponse
     {
+        $place = $request->validated()['place'];
         $results = $this->geoCodeService->search($place);
         return response()->json($results);
     }
 
-    public function getRoute(Request $request): JsonResponse
+    /**
+     * Search place by coordinates.
+     *
+     * @param ReverseSearchRequest $request
+     * @return JsonResponse
+     * @throws ConnectionException|RequestException
+     */
+    public function reverseSearchPlace(ReverseSearchRequest $request): JsonResponse
     {
-        // TODO
+        $coordinates = [
+            'lon' => $request->validated()['lon'],
+            'lat' => $request->validated()['lat']
+        ];
+        $results = $this->geoCodeService->reverseSearch($coordinates);
+        return response()->json($results);
+    }
+
+    /**
+     * Get route between multiple coordinates.
+     *
+     * @param GetRouteRequest $request
+     * @return JsonResponse
+     * @throws ConnectionException|RequestException
+     */
+    public function getRoute(GetRouteRequest $request): JsonResponse
+    {
+        $coordinates = $request->validated()['coordinates'];
+        $results = $this->directionsService->route($coordinates);
+        return response()->json($results);
     }
 }
