fcc5ceb0fec34191b34b0e89dafa67f6a6c64309
AxelReviron <axel-reviron@proton.me>
2025-06-26 09:53:18 +0200

    refacto dto

--- app/Services/OpenRouteService/Dto/LocationFeatureDTO.php
+++ app/Services/OpenRouteService/Dto/LocationFeatureDTO.php
@@ -4,8 +4,16 @@ namespace App\Services\OpenRouteService\Dto;
 
 class LocationFeatureDTO
 {
+    /**
+     * @param string $name
+     * @param string $label
+     * @param float $longitude
+     * @param float $latitude
+     * @param string $country
+     * @param string $region
+     * @param string $locality
+     */
     public function __construct(
-        public string $id,
         public string $name,
         public string $label,
         public float $longitude,
@@ -18,14 +26,13 @@ class LocationFeatureDTO
     public static function fromArray(array $feature): self
     {
         return new self(
-            id: $feature['properties']['id'] ?? '',
-            name: $feature['properties']['name'] ?? '',
-            label: $feature['properties']['label'] ?? '',
-            longitude: $feature['geometry']['coordinates'][0] ?? 0.0,
-            latitude: $feature['geometry']['coordinates'][1] ?? 0.0,
-            country: $feature['properties']['country'] ?? '',
-            region: $feature['properties']['region'] ?? '',
-            locality: $feature['properties']['locality'] ?? '',
+            name: $feature['properties']['name'],
+            label: $feature['properties']['label'],
+            longitude: $feature['geometry']['coordinates'][0],
+            latitude: $feature['geometry']['coordinates'][1],
+            country: $feature['properties']['country'],
+            region: $feature['properties']['region'],
+            locality: $feature['properties']['locality'],
         );
     }
 }
