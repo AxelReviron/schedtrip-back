312672314160c9ea27320173ecf71e18b04c2f1c
AxelReviron <axel-reviron@proton.me>
2025-06-28 22:27:38 +0200

    PrivacyPolicy and TermOfService pages

--- resources/js/Pages/TermsOfService.vue
+++ resources/js/Pages/TermsOfService.vue
@@ -6,9 +6,9 @@ const {t} = useI18n();
 </script>
 
 <template>
-    <h2 class="text-4xl font-bold text-dark">
+    <h1 class="text-4xl font-bold text-dark">
         {{ $t("terms_of_service.title") }}
-    </h2>
+    </h1>
 </template>
 
 <style scoped>
