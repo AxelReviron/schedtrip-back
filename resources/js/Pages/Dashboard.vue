a1236cfdc5e3fd31c3be8084f0140b6b0cf5edaa
AxelReviron <axel-reviron@proton.me>
2025-06-28 22:26:56 +0200

    get auth user

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,6 +1,12 @@
 <script setup lang="ts">
 
-console.log('test');
+import {usePage} from "@inertiajs/vue3";
+import {computed} from "vue";
+
+const page = usePage()
+const user = computed(() => page.props.auth.user)
+
+console.log(user.value);
 </script>
 
 <template>
