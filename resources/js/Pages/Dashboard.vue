1ab5cfdfd075e867e92cecfb6d177d1c30af53df
AxelReviron <axel-reviron@proton.me>
2025-06-28 22:25:26 +0200

    Dashboard page

---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+console.log('test');
+</script>
+
+<template>
+    <h1 class="text-4xl font-bold text-dark">
+        Dashboard
+    </h1>
+</template>
+
+<style scoped>
+
+</style>
