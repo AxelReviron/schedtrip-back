c9281a56929ea033a82c13fbb8654b24845cfcf4
AxelReviron <axel-reviron@proton.me>
2025-06-29 21:17:43 +0200

    Friend cards and modal

---
+++ resources/js/Components/Friends/FriendRequestCard.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { Check, X } from "lucide-vue-next";
+</script>
+
+<template>
+    <div class="bg-gray-100/20 border border-gray-200 rounded-sm px-4 py-4 flex flex-row justify-between shadow-xs">
+        <div class="flex justify-start gap-4 items-center">
+            <div class="relative inline-flex items-center justify-center w-12 h-12 overflow-hidden bg-cream rounded-full">
+                <span class="font-medium text-dark text-lg">J</span>
+            </div>
+            <div class="text-dark">
+                <p class="font-medium">John</p>
+                <p>john.doe@mail.com</p>
+            </div>
+        </div>
+        <div class="flex justify-start gap-2 items-center">
+            <div class="relative inline-flex items-center justify-center w-8 h-8 overflow-hidden bg-green-200 hover:bg-green-300 cursor-pointer rounded-lg">
+                <Check
+                    class="text-green-600"
+                    size="20"
+                />
+            </div>
+            <div class="relative inline-flex items-center justify-center w-8 h-8 overflow-hidden bg-red-200 hover:bg-red-300 cursor-pointer rounded-lg">
+                <X
+                    class="text-red-600"
+                    size="20"
+                />
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ resources/js/Components/Friends/FriendRequestModal.vue
@@ -0,0 +1,132 @@
+<script setup lang="ts">
+import {UserRoundPlus, X} from "lucide-vue-next";
+import {useI18n} from "vue-i18n";
+import {ref} from "vue";
+import axios from "axios";
+import {usePage} from "@inertiajs/vue3";
+import Notification from "@/Components/Notification.vue";
+
+const page = usePage()
+const {t} = useI18n();
+
+const emit = defineEmits(['toggle-visibility']);
+
+const formData = ref({
+    pseudo: '',
+});
+
+const notification = ref({
+    show: false,
+    message: '',
+    type: 'success' as 'success' | 'error'
+});
+
+const errors = ref({});
+
+function showNotification(message: string, type: 'success' | 'error') {
+    notification.value = {
+        show: true,
+        message,
+        type
+    };
+
+    setTimeout(() => {
+        notification.value.show = false;
+    }, 10000);
+}
+
+async function handleSubmit(e: Event) {
+    e.preventDefault();
+    errors.value = {};
+
+    try {
+        const response = await axios.get(`api/users/pseudo/${formData.value.pseudo}`, {
+            _token: page.props.csrf_token,
+        });
+
+        await axios.post('api/users/friends/send', {
+            _token: page.props.csrf_token,
+            user_id: response.data.user_id
+        });
+
+        formData.value = {
+            pseudo: '',
+        };
+
+        showNotification(t("friend.form.notification.success"), 'success');
+    } catch (error: any) {
+        if (error.response && error.response.status === 422) {// Validation errors
+            errors.value = error.response.data.errors;
+            showNotification(t("friend.form.notification.error.form"), 'error');
+        } else if (error.response && error.response.status === 401) {// TODO: error user not found
+            showNotification(t("form.auth.notification.error.credentials"), 'error');
+        } else {// Other errors
+            showNotification(t("friend.form.notification.error.server"), 'error');
+        }
+    }
+}
+</script>
+
+<template>
+
+    <div class="fixed inset-0 z-50 flex items-center justify-center">
+        <div class="z-99 absolute bg-black/20 backdrop-blur-[0.1rem] w-full h-full"></div>
+
+        <div class="z-100 w-100 bg-white border border-gray-200 rounded-sm px-4 py-4 shadow-lg">
+
+            <div class="flex flex-row justify-between">
+                <div
+                    class="flex flex-row gap-2 items-center text-dark font-medium">
+                    <UserRoundPlus
+                        size="24"
+                    />
+                    <h3 class="text-xl">{{ $t("friend.add_friend") }}</h3>
+                </div>
+                <div class="relative inline-flex items-center justify-center w-8 h-8 overflow-hidden cursor-pointer rounded-lg">
+                    <X
+                        @click="emit('toggle-visibility')"
+                        class="text-dark"
+                        size="20"
+                    />
+                </div>
+            </div>
+
+            <div>
+                <form class="text-dark flex flex-col items-center" name="login" method="POST" @submit="handleSubmit">
+                    <div class="flex flex-col mt-2 w-full">
+                        <label for="pseudo" class="p-1 font-medium text-lg">{{ $t("friend.form.pseudo") }}</label>
+                        <input
+                            type="text"
+                            v-model="formData.pseudo"
+                            :placeholder="t('friend.form.pseudo')"
+                            class="bg-white/70 border border-warm p-2 rounded-lg text-warm focus:outline-warm"
+                            name="pseudo" required
+                            id="pseudo"
+                        >
+                        <div v-if="errors.pseudo" class="text-red-500 text-sm mt-1">{{ errors.pseudo[0] }}</div>
+                        <div v-if="errors.user_id" class="text-red-500 text-sm mt-1">{{ errors.user_id[0] }}</div>
+                    </div>
+                    <div class="flex flex-row gap-2 justify-between w-full">
+                        <button
+                            type="submit"
+                            class="mt-4 py-2 px-4 w-full border border-warm bg-warm text-light font-medium rounded-lg px-4 cursor-pointer hover:bg-warmer"
+                        >
+                            {{ $t("friend.form.send") }}
+                        </button>
+                        <button
+                            @click="emit('toggle-visibility')"
+                            class="mt-4 py-2 px-4 w-full border border-warm border-2 rounded-lg text-warm font-medium cursor-pointer"
+                        >
+                            {{ $t("friend.form.cancel") }}
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <Notification :notification="notification"/>
+</template>
+
+<style scoped>
+
+</style>
