59a8dbf3a3bd8750b4bef4e22bdd7024fff91dca
AxelReviron <axel-reviron@proton.me>
2025-06-30 18:58:15 +0200

    Add locales

--- resources/js/locales/en/friend.ts
+++ resources/js/locales/en/friend.ts
@@ -3,20 +3,45 @@ export default {
         title: "Friends",
         subtitle: "Connect with your friends and explore their adventures.",
         add_friend: "Add Friend",
+        delete_friend_request: "Delete friend request",
         friend_requests: 'Friend Requests',
         my_friends: 'My Friends',
+        no_friend: 'No friends yet.',
+        travel_net: 'Start building your travel network by adding friends.',
         form: {
-            pseudo: "Pseudo",
-            send: "Send",
-            cancel: "Cancel",
-            email: "Email",
-            notification: {
-                success: "Friend request successfully sent !",
-                error: {
-                    form: "Please correct the errors in the form.",
-                    server: "An unexpected error occurred. Please try again.",
+            add_friend: {
+                pseudo: "Pseudo",
+                send: "Send",
+                cancel: "Cancel",
+                email: "Email",
+                notification: {
+                    success: "Friend request successfully sent !",
+                    error: {
+                        form: "Please correct the errors in the form.",
+                        server: "An unexpected error occurred. Please try again.",
+                    }
                 }
-            }
+            },
+            reject_friend_request: {
+                subtitle: "You can block this friend request to prevent this user from sending you any more.",
+                reject: "Reject",
+                block: "Block",
+                notification: {
+                    rejected_success: "Friend request successfully rejected !",
+                    blocked_success: "Friend request successfully blocked !",
+                    error: {
+                        server: "An unexpected error occurred. Please try again.",
+                    }
+                }
+            },
+            accept_friend_request: {
+                notification: {
+                    success: "Friend request successfully accepted !",
+                    error: {
+                        server: "An unexpected error occurred. Please try again.",
+                    }
+                }
+            },
         },
     }
 }
--- resources/js/locales/fr/friend.ts
+++ resources/js/locales/fr/friend.ts
@@ -3,20 +3,45 @@ export default {
         title: "Amis",
         subtitle: "Connectez-vous avec vos amis et découvrez leurs aventures.",
         add_friend: "Ajouter un ami",
+        delete_friend_request: "Supprimer la demande d\'ami",
         friend_requests: 'Demandes d\'amis',
         my_friends: 'Mes amis',
+        no_friend: 'Pas encore d\'amis.',
+        travel_net: 'Commencez a construire votre réseaux de voyageurs en ajoutant des amis.',
         form: {
-            pseudo: "Pseudo",
-            send: "Envoyer",
-            cancel: "Annuler",
-            email: "Email",
-            notification: {
-                success: "Demande d\'ami envoyé !",
-                error: {
-                    form: "Veuillez corriger les erreurs dans le formulaire.",
-                    server: "Une erreur inattendue s'est produite. Veuillez réessayer.",
+            add_friend: {
+                pseudo: "Pseudo",
+                send: "Envoyer",
+                cancel: "Annuler",
+                email: "Email",
+                notification: {
+                    success: "Demande d\'ami envoyé !",
+                    error: {
+                        form: "Veuillez corriger les erreurs dans le formulaire.",
+                        server: "Une erreur inattendue s'est produite. Veuillez réessayer.",
+                    }
                 }
-            }
+            },
+            reject_friend_request: {
+                subtitle: "Vous pouvez bloquer cette demande d'ami pour empêcher cet utilisateur de vous en envoyer d'autres.",
+                reject: "Rejeter",
+                block: "Bloquer",
+                notification: {
+                    rejected_success: "La demande d'ami a correctement été rejeté !",
+                    blocked_success: "La demande d'ami a correctement été bloqué !",
+                    error: {
+                        server: "Une erreur inattendue s'est produite. Veuillez réessayer.",
+                    }
+                }
+            },
+            accept_friend_request: {
+                notification: {
+                    success: "La demande d'ami a correctement été accepté !",
+                    error: {
+                        server: "Une erreur inattendue s'est produite. Veuillez réessayer.",
+                    }
+                }
+            },
         },
     }
 }
