b4d01a59505c8dcc06d77b30ba7189a5c2a025b4
AxelReviron <axel-reviron@proton.me>
2025-07-01 21:29:30 +0200

    add trip store

---
+++ resources/js/stores/tripStore.ts
@@ -0,0 +1,39 @@
+import {defineStore} from "pinia";
+import {ref} from "vue";
+import {usePage} from "@inertiajs/vue3";
+import TripInterface from "@/interfaces/tripInterface";
+
+export const useTripStore = defineStore('trip', () => {
+    const trips: TripInterface[]|[] = ref([]);
+
+    const isLoading: boolean = ref(false);
+    const page = usePage();
+
+    async function setTrips(tripData: TripInterface) {
+        trips.value.push(tripData);
+    }
+
+    function addTrip(): void {
+        //
+    }
+
+    function removeTrip(): void {
+        //
+    }
+
+    function updateTrip(): void {
+        //
+    }
+
+    return {
+        // States
+        trips,
+        isLoading,
+
+        // Actions
+        setTrips,
+        addTrip,
+        removeTrip,
+        updateTrip,
+    }
+})
