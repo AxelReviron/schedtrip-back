25f5f0c4b1353db1ba112cea2ac42c755a2eea4e
AxelReviron <axel-reviron@proton.me>
2025-07-03 00:39:46 +0200

    Drag and drop Stop cards (handle order_index).

--- resources/js/components/Trip/Destinations.vue
+++ resources/js/components/Trip/Destinations.vue
@@ -1,15 +1,27 @@
 <script setup lang="ts">
-import {MapPinned, Search} from "lucide-vue-next";
+import {MapPinned, Search, Globe, Grip} from "lucide-vue-next";
 import {useI18n} from "vue-i18n";
 import TitleIcon from "@/components/TitleIcon.vue";
 import {useTripFormStore} from "@/stores/tripFormStore";
 import {storeToRefs} from "pinia";
+import {VueDraggable} from "vue-draggable-plus";
+import {ref} from "vue";
 
 const {t} = useI18n();
 
 const tripFormStore = useTripFormStore();
-const { stops } = storeToRefs(tripFormStore);
+const {trip} = storeToRefs(tripFormStore);
+const localStops = ref([...trip.value.stops]);
 
+function updateStopOrder() {
+    localStops.value.forEach((stop, index) => {
+        stop.order_index = index;
+    });
+
+    trip.value.stops = [...localStops.value];// TODO: fonction store
+    // TODO: Mettre a jour les markers et itineraire
+}
+// TODO: Recherche et gérer affichage
 </script>
 
 <template>
@@ -20,7 +32,7 @@ const { stops } = storeToRefs(tripFormStore);
                 :icon="MapPinned"
             />
             <h4 class="text-dark text-[1rem]">
-                {{ stops.length }} {{ $t("trip.form.create_trip.destinations.stops") }}
+                {{ trip.stops.length }} {{ $t("trip.form.create_trip.destinations.stops") }}
             </h4>
         </div>
 
@@ -32,7 +44,8 @@ const { stops } = storeToRefs(tripFormStore);
                 name="search" required
                 id="search"
             >
-            <button class="flex flex-row gap-2 items-center border border-warm py-2 bg-warm font-medium rounded-sm px-4 cursor-pointer hover:bg-warmer">
+            <button
+                class="flex flex-row gap-2 items-center border border-warm py-2 bg-warm font-medium rounded-sm px-4 cursor-pointer hover:bg-warmer">
                 <Search
                     class="text-white"
                     size="20"
@@ -40,9 +53,51 @@ const { stops } = storeToRefs(tripFormStore);
             </button>
         </div>
 
-        <div v-if="stops.length > 0">
+        <div class="w-full border border-1 my-4 rounded-sm text-warm opacity-30"></div>
 
-
+        <div v-if="trip.stops && trip.stops.length > 0">
+            <VueDraggable
+                v-model="localStops" @end="updateStopOrder"
+                handle=".handle" ghostClass="ghost"
+            >
+                <div
+                    v-for="stop in trip.stops" :key="stop.id"
+                    class="bg-white border border-warm mt-2 rounded-sm px-4 py-4 shadow-xs flex flex-row gap-2"
+                >
+                    <Grip
+                        class="text-warm hover:text-warmer hover:cursor-grab handle"
+                    />
+                    <div class="w-full">
+                        <input
+                            type="text"
+                            v-model="stop.label"
+                            :placeholder="t('trip.form.create_trip.destinations.destination_placeholder')"
+                            class="bg-white/70 border border-warm p-2 rounded-sm text-dark focus:outline-warm w-full"
+                            name="destination"
+                            id="destination"
+                        />
+                        <div v-if="stop.notes && stop.notes.length > 0">
+                            <div v-for="(note, index) in stop.notes" :key="note.id">
+                            <textarea
+                                rows="2"
+                                v-model="note.content"
+                                :placeholder="t('trip.form.create_trip.destinations.note_placeholder')"
+                                class="mt-2 w-full border border-warm p-2 rounded-lg text-dark focus:outline-warm resize-none"
+                                name="note" required
+                                id="note"
+                            >
+                            </textarea>
+                            </div>
+                        </div>
+                        <div class="text-warm text-sm flex flex-row items-center gap-2">
+                            <Globe
+                                size="18"
+                            />
+                            <p>{{ stop.latitude }}, {{ stop.longitude }}</p>
+                        </div>
+                    </div>
+                </div>
+            </VueDraggable>
         </div>
 
         <div v-else class="flex flex-col items-center justify-center mt-8">
@@ -61,5 +116,8 @@ const { stops } = storeToRefs(tripFormStore);
 </template>
 
 <style scoped>
-
+.ghost {
+    opacity: 0.5;
+    background: #FAEDCD;
+}
 </style>
