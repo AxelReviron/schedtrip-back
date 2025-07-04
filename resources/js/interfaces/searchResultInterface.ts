167312adbaf21654e59236d6d399e91571c03d9b
AxelReviron <axel-reviron@proton.me>
2025-07-04 18:43:35 +0200

    Add searchResultInterface

---
+++ resources/js/interfaces/searchResultInterface.ts
@@ -0,0 +1,9 @@
+export default interface SearchResultInterface {
+    country: string;
+    label: string;
+    latitude: number;
+    locality: string;
+    longitude: number;
+    name: string;
+    region: string;
+}
