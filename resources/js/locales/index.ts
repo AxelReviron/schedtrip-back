b6395b4f2a00daff503121213dc11122a047d43f
AxelReviron <axel-reviron@proton.me>
2025-07-05 18:38:14 +0200

    Add navbar locales

--- resources/js/locales/index.ts
+++ resources/js/locales/index.ts
@@ -3,12 +3,15 @@ import frPrivacyPolicy from './fr/privacyPolicy';
 import frTermsOfService from './fr/termsOfService';
 import frFriend from './fr/friend';
 import frTrip from './fr/trip';
+import frNavbar from './fr/navbar';
 
 import enHome from './en/home'
 import enPrivacyPolicy from './en/privacyPolicy';
 import enTermsOfService from './en/termsOfService';
 import enFriend from './en/friend';
 import enTrip from './en/trip';
+import enNavbar from './en/navbar';
+
 
 export default {
     fr: {
@@ -16,13 +19,15 @@ export default {
         ...frPrivacyPolicy,
         ...frTermsOfService,
         ...frFriend,
-        ...frTrip
+        ...frTrip,
+        ...frNavbar,
     },
     en: {
         ...enHome,
         ...enPrivacyPolicy,
         ...enTermsOfService,
         ...enFriend,
-        ...enTrip
+        ...enTrip,
+        ...enNavbar,
     }
 }
