4f2400203bb689bdae337493f0068b9f8cf98f74
AxelReviron <axel-reviron@proton.me>
2025-07-06 14:32:06 +0200

    create trip

--- resources/js/pages/CreateTrip.vue
+++ resources/js/pages/CreateTrip.vue
@@ -3,7 +3,7 @@ import Navbar from "@/components/Navbar.vue";
 import { Save } from "lucide-vue-next";
 import HeroBanner from "@/components/HeroBanner.vue";
 import {useI18n} from "vue-i18n";
-import {ref} from "vue";
+import {ref, watch} from "vue";
 import {storeToRefs} from "pinia";
 import {useUserStore} from "@/stores/userStore";
 import InteractiveMap from "@/components/Trip/InteractiveMap.vue";
@@ -11,10 +11,10 @@ import Destinations from "@/components/Trip/Destinations.vue";
 import TripDetails from "@/components/Trip/TripDetails.vue";
 import TripParticipants from "@/components/Trip/TripParticipants.vue";
 import {useNotification} from "@/composables/useNotification";
-import axios from "axios";
 import {useTripFormStore} from "@/stores/tripFormStore";
 import {usePage} from "@inertiajs/vue3";
 import Notification from "@/components/Notification.vue";
+import axios from "axios";
 
 const page = usePage();
 const {t} = useI18n();
@@ -22,23 +22,8 @@ const {t} = useI18n();
 const userStore = useUserStore();
 const { user } = storeToRefs(userStore);
 
-const isModalVisible = ref(false);
-
 const tripFormStore = useTripFormStore();
-const {
-    label,
-    description,
-    is_public,
-    distance,
-    duration,
-    geojson,
-    participants,
-    stops
-} = storeToRefs(tripFormStore);
-
-function handleRemoveTripModalVisibility() {
-    isModalVisible.value = !isModalVisible.value;
-}
+const { trip } = storeToRefs(tripFormStore);
 
 const errors = ref({});
 const { notification, showNotification } = useNotification();
@@ -48,36 +33,54 @@ async function createTrip(e: Event) {
     errors.value = {};
 
     try {
-        const response = await axios.post('/api/trips', {
+        const tripResponse = await axios.post('/api/trips', {//TODO
             _token: page.props.csrf_token,
-            label: label.value,
-            description: description.value,
-            is_public: is_public.value,
-            distance: distance.value,
-            duration: duration.value,
-            geojson: geojson.value,
-            author: '/api/users/' + user.value.id,
+            label: trip.value.label,
+            description: trip.value.description,
+            is_public: trip.value.isPublic,
+            distance: trip.value.distance,
+            duration: trip.value.duration,
+            geojson: JSON.stringify(trip.value.geojson),
+            author: trip.value.author,
         }, {
             headers: {
                 'Content-Type': 'application/ld+json'
             }
         });
 
-        const tripId = response.data.id;
+        const tripId = tripResponse.data.id;
 
-        /*await axios.post(`api/trips/${tripId}/participants`, {
-            _token: page.props.csrf_token,
-            participants: [
-                {
-                    user_id: '',
-                    user_id: 'view'
+        for (const stop of trip.value.stops) {
+            await axios.post('/api/stops', {
+                _token: page.props.csrf_token,
+                label: stop.label,
+                description: stop.description,
+                latitude: stop.latitude,
+                longitude: stop.longitude,
+                arrivalDate: stop.arrival_date,
+                departureDate: stop.departure_date,
+                orderIndex: stop.order_index,
+                trip: `/api/trips/${tripId}`
+            }, {
+                headers: {
+                    'Content-Type': 'application/ld+json'
                 }
-            ]
-        });*/
+            });
+        }
+
+        if (trip.value.participants && trip.value.participants.length > 0) {
+            await axios.post(`/api/trips/${tripId}/participants`, {
+                _token: page.props.csrf_token,
+                participants: trip.value.participants
+            });
+        }
+
 
         showNotification(t("trip.form.create_trip.notification.success"), 'success');
+        //TODO: Redirect to edit page
     } catch (error: any) {
         console.log(error);
+        //TODO: Manage errors
         //if (error.response && error.response.status === 422) {// Validation errors
         //    errors.value = error.response.data.errors;
         //    showNotification(t("friend.form.notification.error.form"), 'error');
@@ -88,15 +91,22 @@ async function createTrip(e: Event) {
         //}
     }
 }
+
+/**
+ * Trigger when user id is defined
+ */
+watch(
+    () => user.value,
+    () => {
+        if (user.value && user.value.id && !trip.value.author) {
+            tripFormStore.setAuthor(user.value.id);
+        }
+    },
+    { deep: true, immediate: true }
+);
 </script>
 
 <template>
-    <!--    <RemoveTripModal-->
-    <!--        :friend-id="friend.id"-->
-    <!--        v-if="isModalVisible"-->
-    <!--        @toggle-visibility="handleRemoveTripModalVisibility"-->
-    <!--    />-->
-
     <Navbar/>
     <div class="bg-light flex flex-col items-center justify-center">
         <div class="w-10/12 xl:w-8/12 mx-auto my-8">
